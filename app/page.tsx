fe6afff192c71c5dfdb06d7304fa276be2a3585d
Jiggy40-cpu <amachree6332@student.babcock.edu.ng>
2026-03-18 22:29:17 +0100

    removed experience page for now

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,6 @@ import Hero from '../components/Hero';
 import About from '../components/About';
 import Skills from '../components/Skills';
 import Projects from '../components/Projects';
-import Experience from '../components/Experience';
 import Testimonials from '../components/Testimonials'
 import Contact from '../components/Contact';
 
@@ -16,7 +15,6 @@ import Contact from '../components/Contact';
     <About />
     <Skills />
     <Projects />
-    <Experience />
     <Testimonials />
     
     <Contact />
