6f8e262b1ebfa17de570fe3f130d10b181ae8f44
Jiggy40-cpu <amachree6332@student.babcock.edu.ng>
2026-03-18 22:14:09 +0100

    changes

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,6 @@ import Skills from '../components/Skills';
 import Projects from '../components/Projects';
 import Experience from '../components/Experience';
 import Testimonials from '../components/Testimonials'
-import CTA from '../components/CTA'
 import Contact from '../components/Contact';
 
 
